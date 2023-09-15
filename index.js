38f526e604e3442616091da3167af3f8b26c5870
Harry Whyte~ <harrywhyte@kubrickgroup.com>
2023-09-15 15:31:26 +0100

    Added bodyparser import

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+let mongoose = require('mongoose'); // Import mongoose requirement for DB
+const bodyParser = require('body-parser'); // Import response body parsing middleware
+
+app.use(bodyParser.urlencoded())
+
+mongoose.connect(process.env['MONGO_URI'], { useNewUrlParser: true, useUnifiedTopology : true});  // Connect to database API
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
-
 app.use(cors());
-
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
@@ -17,6 +21,11 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.post('/api/shorturl', function(req, res) {
+  
+  
 });
 
 app.listen(port, function() {
